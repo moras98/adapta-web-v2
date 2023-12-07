5c4d50799ef0c4d87f28b539edc602627ee7296b
Agustin Moras <a.morascastro@gmail.com>
2023-12-07 17:29:27 -0300

    change src category

--- src/routes/Contact/Category/Category.js
+++ src/routes/Contact/Category/Category.js
@@ -6,16 +6,24 @@ import { useParams } from "react-router-dom";
 
 export default function Category({categories}){
     const { category } = useParams();
+    const currentPath = window.location.pathname;
     const language = categories[category];
     let h1 = '';
+    let src = '/assets/images/banner.jpg';
+
     if (language.areas.length > 1) {
         h1 = language.title
+        src = language.src
     } else {
-        h1 = 'Experiencia'
+        if (currentPath.includes('/en')){
+            h1 = 'Experience';
+        } else {
+            h1 = 'Experiencia'
+        }
     }
     return(
         <div className="body">
-            <ImageFrame src="/assets/images/banner.jpg" h1={h1} h2=""/>
+            <ImageFrame src={src} h1={h1} h2=""/>
             <div className="content">
                 <div className="phrase-section">
                     <h2>{language.phrase}</h2>
