1f0a2dae75cfc7cc63e7112283b310f2e96aeed1
Agustin Moras <a.morascastro@gmail.com>
2024-09-13 16:25:22 -0300

    change area number

--- src/components/TeamMembers/TeamMemberGrid.js
+++ src/components/TeamMembers/TeamMemberGrid.js
@@ -5,17 +5,17 @@ import TeamMember from "./TeamMember";
 export default function TeamMemberGrid(){
     const members = [
         {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
-        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 1'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 2'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 3'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 4'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 5'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 6'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 7'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 8'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 9'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 10'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 11'},
+        {image: "/assets/images/silueta.png", name: 'Nombre Apellido', area: 'Area 12'},
     ];
 
     return(
